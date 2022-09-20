d35d4e14bdb72c63925b29bd83a9e826f3d2a966
Chhuong Le <chhuongle@gmail.com>
2022-09-20 14:05:22 -0700

    complete functions for agent buttons

--- src/agentTiles/agentTile.jsx
+++ src/agentTiles/agentTile.jsx
@@ -1,16 +1,33 @@
 import React, {useState} from 'react';
+import agentData from '../agents.json';
 import "./agentTile.css";
 
 const AgentTile = () => {
-  const [agentName, setAgentName] = useState('None');
-  const [agentIcon, setAgentIcon] = useState('None');
+  const [agentName, setAgentName] = useState(agentData.agents[0].agentName);
+  const [agentIcon, setAgentIcon] = useState(agentData.agents[0].agentProfile);
+  const agentListSize = agentData.agents.length;
+  const agentArr = agentData.agents;
+
+  const randomAgent = () => {
+    let agentID = Math.random() * (agentListSize - 1) + 1;
+    agentID = Math.floor(agentID);
+
+    setAgentName(agentArr[agentID].agentName);
+    setAgentIcon(agentArr[agentID].agentProfile);
+  }
+
+  const clearAgent = () => {
+    setAgentName(agentArr[0].agentName);
+    setAgentIcon(agentArr[0].agentProfile);
+  }
+
   return (
     <div className="Agent-Tile">
       <div className="Heading">Agent Name</div>
       <img className="Agent-Image" src={agentIcon}/>
       <div className="Agent-Name">{agentName}</div>
-      <button className="Random">Random Agent</button>
-      <button className="Clear">Clear</button>
+      <button className="Random" onClick={randomAgent}>Random Agent</button>
+      <button className="Clear" onClick={clearAgent}>Clear</button>
     </div>
   );
 }
