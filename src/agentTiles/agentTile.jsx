1447c4fe5f7eef4957c4251499fd24e94baeeb56
Chhuong Le <chhuongle@gmail.com>
2022-09-20 13:24:56 -0700

    fix files

--- src/agentTiles/agentTile.jsx
+++ src/agentTiles/agentTile.jsx
@@ -7,8 +7,8 @@ const AgentTile = () => {
   return (
     <div className="Agent-Tile">
       <div className="Heading">Agent Name</div>
-      <img className="Agent-Image" src="https://static.wikia.nocookie.net/valorant/images/3/35/Jett_icon.png/"/>
-      <div className="Agent-Name">Jett</div>
+      <img className="Agent-Image" src={agentIcon}/>
+      <div className="Agent-Name">{agentName}</div>
       <button className="Random">Random Agent</button>
       <button className="Clear">Clear</button>
     </div>
