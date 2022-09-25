e60d897449b940aaff5c4f23b9ddda778339572c
77046302+ChhuongLe@users.noreply.github.com <chhuongcle@gmail.com>
2022-09-25 13:46:48 -0700

    develop method to avoid overlapping agents

--- src/MapSelector/MapSelector.jsx
+++ src/MapSelector/MapSelector.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import './MapSelector.css';
 import defaultImage from '../images/default.jpeg';
+import agentData from '../agents.json';
 import mapData from '../map.json';
 import AgentTile from '../agentTiles/agentTile'
 
@@ -9,6 +10,8 @@ const MapSelector = () => {
   const [mapName, setMapName] = useState('None');
   const mapDataSize = mapData.maps.length;
   const mapArr = mapData.maps;
+  // set up a hash to store all the agents that have been selected
+  let pickedAgents = new Map();
 
   // select random index from the mapData
   const randomize = () => {
@@ -30,11 +33,11 @@ const MapSelector = () => {
   return (
     <div className="Map-Selector">
       <div className="Attacking-Agents">
-        <AgentTile className="Agent-1"/>
-        <AgentTile className="Agent-2"/>
-        <AgentTile className="Agent-3"/>
-        <AgentTile className="Agent-4"/>
-        <AgentTile className="Agent-5"/>
+        <AgentTile className="A-Agent" idx={1} pickedAgents={pickedAgents}/>
+        <AgentTile className="A-Agent" idx={2} pickedAgents={pickedAgents}/>
+        <AgentTile className="A-Agent" idx={3} pickedAgents={pickedAgents}/>
+        <AgentTile className="A-Agent" idx={4} pickedAgents={pickedAgents}/>
+        <AgentTile className="A-Agent" idx={5} pickedAgents={pickedAgents}/>
       </div>
 
       <img className="Map-Image" src={background} alt='N/A'/>
--- src/agentTiles/agentTile.jsx
+++ src/agentTiles/agentTile.jsx
@@ -3,18 +3,25 @@ import agentData from '../agents.json';
 import empty from '../images/emptyProfile.png';
 import "./agentTile.css";
 
-const AgentTile = () => {
+const AgentTile = ({pickedAgents, idx}) => {
   const [agentName, setAgentName] = useState('None');
   const [agentIcon, setAgentIcon] = useState(empty);
   const agentListSize = agentData.agents.length;
   const agentArr = agentData.agents;
 
+
   const randomAgent = () => {
     let agentID = Math.random() * (agentListSize - 1) + 1;
     agentID = Math.floor(agentID);
 
-    setAgentName(agentArr[agentID].agentName);
-    setAgentIcon(agentArr[agentID].agentProfile);
+    if(Array.from(pickedAgents.values()).includes(agentID)) {
+      console.log("Triggered");
+      randomAgent();
+    } else {
+      pickedAgents.set(idx, agentID);
+      setAgentName(agentArr[agentID].agentName);
+      setAgentIcon(agentArr[agentID].agentProfile);
+    }
   }
 
   const clearAgent = () => {
